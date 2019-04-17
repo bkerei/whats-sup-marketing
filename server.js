6f7cfccdba78caba7e44822caf62411ef4ca029c
bkerei <bwkerei@gmail.com>
2019-04-17 16:08:18 +1200

    updated pages and added page template

--- server.js
+++ server.js
@@ -2,11 +2,23 @@ const express = require('express')
 
 const server = express()
 
+server.use('/dist', express.static('dist'))
 
-server.use("/dist",express.static("dist"))
+server.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html')
+})
 
-server.get("/",(req,res)=>{
-    res.sendFile(__dirname+"/index.html")
+server.get('/faq', (req, res) => {
+  res.sendFile(__dirname + '/faq.html')
+})
+server.get('/privacy', (req, res) => {
+  res.sendFile(__dirname + '/privacy.html')
+})
+server.get('/contact', (req, res) => {
+  res.sendFile(__dirname + '/contact.html')
+})
+server.get('/about', (req, res) => {
+  res.sendFile(__dirname + '/about.html')
 })
 
 module.exports = server
